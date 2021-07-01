ea7c2a2798386b3e340bccfdb83580a7192b400e
pedromesmer <pedro_mesmer@hotmail.com>
2021-07-01 11:34:53 -0300

    implementadas rotas com react nativatio

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Container } from './style';
+
+const SignIn: React.FC = () => {
+  console.log('epa pera lá');
+  return <Container />;
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Container } from './style';
+
+const SignUp: React.FC = () => {
+  console.log('epa pera lá');
+  return <Container />;
+};
+
+export default SignUp;
