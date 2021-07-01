970f935b6e9d441758b0e36602ee9960fd605e24
pedromesmer <pedro_mesmer@hotmail.com>
2021-07-01 15:28:33 -0300

    implementados componentes Button e Input

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './style';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './style';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
 
     <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button
+      onPress={() => {
+        console.log('pegou');
+      }}
+    >
+      Entrar
+    </Button>
   </Container>
 );
 
